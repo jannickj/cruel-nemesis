58313f7747e7267d52b751d28086abadc56f4d1e
jannick <jannickboese@msn.com>
2013-10-11 07:49:48 +0200

    Changed to meta file system

---
+++ UnityTestGame/Assets/UnityLogic/GlobalGameSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.GameLogic;
+
+namespace Assets.UnityLogic
+{
+	public class GlobalGameSettings
+	{
+        private Player mainPlayer;
+        private List<Player> localPlayers = new List<Player>();
+
+
+        public Player MainPlayer
+        {
+            get { return mainPlayer; }
+         }
+
+        public Player[] LocalPlayers
+        {
+            get { return localPlayers.ToArray(); }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            this.localPlayers.Add(player);
+        }
+
+        void Start()
+        {
+            Player p = new Player();
+            p.Name = "player 1";
+            this.mainPlayer = p;
+            this.AddPlayer(p);
+        }
+
+
+	}
+}
---
+++ UnityTestGame/Assets/UnityLogic/GuiLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.UnityLogic
+{
+	public class GuiLoader : MonoBehaviour
+	{
+        public GUITexture PlayerLogo;
+        public GUITexture[] Phases;
+        public GUITexture HealthBar;
+        public GUITexture ManaCrystalFury;
+
+        void Start()
+        {
+            Instantiate(PlayerLogo);
+        }
+	}
+}
