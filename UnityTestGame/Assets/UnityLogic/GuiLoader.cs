6367557bef97bce3c779d395e1c8e9e9799c335f
jannick <jannickboese@msn.com>
2013-10-11 17:19:45 +0200

    Added debugging friendly files

--- UnityTestGame/Assets/UnityLogic/GuiLoader.cs
+++ UnityTestGame/Assets/UnityLogic/GuiLoader.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using XmasEngineModel;
+using Assets.GameLogic.Events;
+using XmasEngineModel.Management;
+using Assets.UnityLogic.Gui;
 
 namespace Assets.UnityLogic
 {
@@ -13,9 +17,34 @@ namespace Assets.UnityLogic
         public GUITexture HealthBar;
         public GUITexture ManaCrystalFury;
 
+
+
         void Start()
         {
-            
+            XmasModel engine = EngineHandler.GetEngine();
+
+            engine.EventManager.Register(new Trigger<PlayerJoinedEvent>(OnPlayerJoin));
         }
+
+        private void OnPlayerJoin(PlayerJoinedEvent evt)
+        {
+            
+            if (GlobalGameSettings.GetSettings().MainPlayer == evt.Player)
+            {
+                GameObject gobj = new GameObject();
+                gobj.AddComponent<PlayerInformation>();
+                PlayerInformation pinfo = gobj.GetComponent<PlayerInformation>();
+
+                pinfo.Player = evt.Player;
+
+                gobj.AddComponent<GuiController>();
+                gobj.AddComponent<GuiViewHandler>();
+
+
+               
+            }
+        }
+
+        
 	}
 }
---
+++ UnityTestGame/Assets/UnityLogic/PlayerInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.GameLogic;
+using UnityEngine;
+
+namespace Assets.UnityLogic
+{
+	public class PlayerInformation : MonoBehaviour
+	{
+        public Player Player { get; set; }
+	}
+}
