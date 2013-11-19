bfbdbbf26c3dd5e6e5ddc268dbe4f8649b8fca14
jannick <jannickboese@msn.com>
2013-11-19 14:19:21 +0100

    Added ability to reset priorities

---
+++ UnityTestGame/Assets/UnityLogic/Game/Cards/BloodwyrmSpawnCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cruel.GameLogic.SpellSystem;
+using Assets.UnityLogic.Game.Modules;
+
+namespace Assets.UnityLogic.Game.Cards
+{
+	public class BloodwyrmSpawnCard : GameCard
+	{
+        public BloodwyrmSpawnCard()
+        {
+            this.RegisterModule(new GraphicsModule("bloodwyrm_spawn"));
+        }
+	}
+}
