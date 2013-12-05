a3220dd57986926a0c9b4a16e4c40ae2485bf9f9
jannick <jannickboese@msn.com>
2013-12-05 13:35:00 +0100

    Fixed GainXPCommand

--- Cruel/Cruel/GameLogic/PlayerCommands/GainXPCommand.cs
+++ Cruel/Cruel/GameLogic/PlayerCommands/GainXPCommand.cs
@@ -11,13 +11,17 @@ namespace Cruel.GameLogic.PlayerCommands
     {
         public Player Player { get; private set; }
         public int XP { get; private set; }
-        public ManaStorage Storage { get; private set; }
 
-        public GainXPCommand(Player player, int Xp, ManaStorage storage)
+        public GainXPCommand(Player player, int Xp)
         {
             this.Player = player;
             this.XP = Xp;
-            this.Storage = storage;
+        }
+
+        protected override void Execute()
+        {
+            //this.EventManager.Raise(new 
+            this.Player.AddXP(XP);
         }
     }
 }
