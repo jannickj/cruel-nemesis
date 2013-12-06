3c8d1e3eff1fc3c699281a8c74aa0cf0ca7d28a4
Philip <philip.oerum@gmail.com>
2013-12-06 19:56:40 +0100

    Adding more cards.

---
+++ UnityTestGame/Assets/UnityLogic/Animations/CardAnimations/WarhoundGraphics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.UnityLogic.Unit;
+
+namespace Assets.UnityLogic.Animations.CardAnimations
+{
+	public class WarhoundGraphics : TextureGraphics
+	{
+        public WarhoundGraphics()
+        {
+    
+        }
+
+        public override void LoadAnimations()
+        {
+            var idle_ani = new TextureAnimation("warhound", 1,1);
+            idle_ani.Frames = Enumerable.Range(1, 1).ToArray();
+            idle_ani.FrameRepeats = Enumerable.Repeat<int>(50, 1).ToArray();
+            this.SetAnimation(StandardUnitAnimations.Idle, idle_ani);
+        }
+    }
+}
