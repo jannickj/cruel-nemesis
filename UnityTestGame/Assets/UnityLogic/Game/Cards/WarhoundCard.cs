9344c9fc3bd05c63a6039ea6178e43aaf243b454
Philip <philip.oerum@gmail.com>
2013-12-14 17:59:52 +0100

    Making cards :)

--- UnityTestGame/Assets/UnityLogic/Game/Cards/WarhoundCard.cs
+++ UnityTestGame/Assets/UnityLogic/Game/Cards/WarhoundCard.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.UnityLogic.Game.Modules;
+using Cruel.GameLogic.SpellSystem;
+using Cruel.Map.Terrain;
+using XmasEngineModel.Management.Actions;
+using Assets.UnityLogic.Game.Units;
 
 namespace Assets.UnityLogic.Game.Cards
 {
-	class WarhoundCard
+	class WarhoundCard : GameCardWithSpellEffect
 	{
+        public WarhoundCard()
+        {
+            this.RegisterModule(new GraphicsModule("warhound"));
+            this.SetTargetCondition(0, obj => obj is TerrainEntity);
+            this.TargetCounts = new int[] { 1 };
+            this.ManaCost = new List<Mana>(new Mana[] { Mana.Fury, Mana.Fury });
+        }
+
+
+        protected override void OnSpellEffect(Spell spell)
+        {
+            var target = (TerrainEntity)spell.Targets[0][0];
+            var unit = new WarhoundUnit(this.Owner);
+            spell.RunAction(new AddEntityAction(unit, target.Position.GenerateSpawn()));
+            
+        }
 	}
 }
---
+++ UnityTestGame/Assets/UnityLogic/Game/Units/ArcherUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cruel.GameLogic;
+using Cruel.GameLogic.Unit;
+using Cruel.GameLogic.Modules;
+
+namespace Assets.UnityLogic.Game.Units
+{
+	public class ArcherUnit : UnitEntity
+    {
+        public ArcherUnit(Player owner) : base(owner)
+        {
+            this.RegisterModule(new MoveModule(5));
+            this.Module<AttackModule>().AttackRange = 5;
+            this.Module<AttackModule>().Damage = 2;
+            this.Module<HealthModule>().MaxHealth = 2;
+            this.Module<HealthModule>().Health = 2;
+        }
+	}
+}
