9825a4ff470ef2a237fef26923f269465ad7e250
jannick <jannickboese@msn.com>
2013-12-20 14:32:17 +0100

    Begun work on networkmode

---
+++ Cruel/CruelServer/CruelServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSLibrary.Network.Server;
+using System.Net.Sockets;
+using JSLibrary.Network;
+
+namespace CruelServer
+{
+    public class CruelServer : MultiClientServer
+    {
+
+        public CruelServer(ServerFactory factory, TcpListener listener) : base(factory, listener)
+        {
+
+        }
+
+        public override ClientConnector CreateConnector()
+        {
+            return new CruelClientConnector();
+        }
+    }
+}
